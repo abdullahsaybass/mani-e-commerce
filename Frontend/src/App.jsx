273da428c07fbd38ed82c6c31792a1ec4b717738
stayindigital-IT <learnwithai8@gmail.com>
2025-06-10 12:40:22 +0530

    add mani-e-commerce master

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './components/Home/Home';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Register from './components/User/Register';
 import Login from './components/User/Login';
 
